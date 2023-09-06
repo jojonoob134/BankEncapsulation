4f463fd37ab4bbec0d14d41c98bac0130019d4ad
Jorge Saldana <jojonoob134@gmail.com>
2023-09-06 13:24:11 -0500

    made a bank

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    internal class BankAccount
+    {
+        private double _balance = 100;
+        public double GetBalance()
+        {
+            return _balance;
+        }
+        public double Deposet(double adding)
+        {
+            _balance += adding;
+            return _balance;
+        }
+
+        public double Withdrawal(double taking)
+        {
+            _balance -= taking;
+            return _balance;
+        }
+    }
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BankAccount account = new BankAccount();
+            bool isInATM = false;
+            while (!isInATM)
+            {
+                Console.WriteLine("press 1 to see your balence, press 2 to deposet some money, press 3 to Withdrawal some money, press 4 to leave");
+                int chose = int.Parse(Console.ReadLine());
+                if (chose == 1)
+                {
+                    Console.WriteLine($"You have {account.GetBalance()} in your bank account");
+                }
+                if (chose == 2)
+                {
+                    Console.WriteLine("how much money do you want to depost");
+                    double addingNum = double.Parse(Console.ReadLine());
+                    account.Deposet(addingNum);
+                }
+                if (chose == 3)
+                {
+                    Console.WriteLine("how much money do you want to depost");
+                    double subtractNum = double.Parse(Console.ReadLine());
+                    account.Deposet(subtractNum);
+                }
+                if (chose == 4)
+                {
+                    isInATM = true;
+                }
+            }
         }
     }
 }
